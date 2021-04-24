deaf846d5169e7370258676072394680f41afb4d
sidrk01 <81598654+sidrk01@users.noreply.github.com>
2021-04-24 16:42:07 -0700

    Added Demo Link

--- turnin/sramk002_lab5_part3.c
+++ turnin/sramk002_lab5_part3.c
@@ -6,6 +6,8 @@
  *
  *      I acknowledge all content contained herein, excluding template or example
  *      code, is my own original work.
+ *      
+ *      Demo Link: https://youtu.be/0vRAlHB6nds
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
